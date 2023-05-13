417a1cdc3d7b01472c5eace25a2b9f1d48266a1a
Aidan Nesbitt <75648657+ajesus7@users.noreply.github.com>
2023-05-13 13:36:48 -0700

    makes the octopus face the cursor

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 import pygame
+import math
 
-class Octopus:
-    def __init__(self, screen):
-        # load octopus image and get its rect
-        self.image = pygame.image.load('octopus.png').convert_alpha()
-        image_size = self.image.get_size()
-        self.rect = self.image.get_rect()
+pygame.init()
 
-        # set octopus position to center of screen
-        self.rect.center = screen.get_rect().center
-
-    def draw(self, screen):
-        # draw octopus on screen
-        screen.blit(self.image, self.rect)
-
+# Set up the screen
+screen_width = 1920
+screen_height = 1080
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("My Game")
 
 class Background:
     def __init__(self, screen):
         self.image = pygame.image.load("starterBackground.png").convert()
         self.image = pygame.transform.scale(self.image, (screen.get_width(), screen.get_height()))
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect(center=screen.get_rect().center)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
-    pygame.display.set_caption('My Game')
+class Octopus:
+    def __init__(self, screen):
+        self.image_orig = pygame.image.load("octopus.png").convert_alpha()
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect(center=screen.get_rect().center)
+        self.angle = 0
 
-    background = Background(screen)
-    octopus = Octopus(screen)
+    def draw(self, screen):
+        self.image = pygame.transform.rotate(self.image_orig, -self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        screen.blit(self.image, self.rect)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    def update(self, mouse_pos):
+        dx, dy = mouse_pos[0] - self.rect.centerx, mouse_pos[1] - self.rect.centery
+        self.angle = 90 + math.degrees(math.atan2(dy, dx))
+
+background = Background(screen)
+octopus = Octopus(screen)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
+    mouse_pos = pygame.mouse.get_pos()
+    octopus.update(mouse_pos)
+    screen.fill((0, 0, 0))
+    background.draw(screen)
+    octopus.draw(screen)
+    pygame.display.flip()
 
-        #draw the background and octopus on the screen
-        background.draw(screen)
-        octopus.draw(screen)
-
-        pygame.display.flip()
-
-    pygame.quit()
+pygame.quit()
 
 if __name__ == '__main__':
     main()
