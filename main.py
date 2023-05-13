cbd49b9f9e24afb25a4c138fdc615a163cfc1229
Aidan Nesbitt <75648657+ajesus7@users.noreply.github.com>
2023-05-13 14:48:58 -0700

    Update main.py
    FIXED FILE

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import math
-import math
 
 animation = 4
 moveSpeed = 5
@@ -10,7 +9,6 @@ screen_height = 1080
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("My Game")
 
-pygame.init()
 class Octopus(pygame.sprite.Sprite):
     def __init__(self, screen):
         pygame.sprite.Sprite.__init__(self)
@@ -26,11 +24,6 @@ class Octopus(pygame.sprite.Sprite):
         self.angle = 0
         self.pos = [screen_width // 2, screen_height // 2]
 
-# Set up the screen
-screen_width = 1920
-screen_height = 1080
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("My Game")
         # set octopus position to center of screen
         self.rect.center = screen.get_rect().center
 
@@ -58,13 +51,6 @@ pygame.display.set_caption("My Game")
         self.rect.centerx += self.movex
         self.rect.centery += self.movey
 
-##        if self.movex < 0:
-  ##          self.frame += 1
-    ##        if self.frame > 3 * animation:
-      ##          self.frame = 0
-        ##    self.image = pygame.transform.flip(self.images[self.frame // animation], True, False)
-
-
 class Background:
     def __init__(self, screen):
         self.image = pygame.image.load("starterBackground.png").convert()
@@ -75,39 +61,14 @@ class Background:
 
         screen.blit(self.image, self.rect)
 
-class Octopus:
-    def __init__(self, screen):
-        self.image_orig = pygame.image.load("octopus.png").convert_alpha()
-        self.image = self.image_orig.copy()
-        self.rect = self.image.get_rect(center=screen.get_rect().center)
-        self.angle = 0
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+    pygame.display.set_caption('My Game')
 
-    def draw(self, screen):
-        self.image = pygame.transform.rotate(self.image_orig, -self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        screen.blit(self.image, self.rect)
+    background = Background(screen)
+    octopus = Octopus(screen)
 
-    def update(self, mouse_pos):
-        dx, dy = mouse_pos[0] - self.rect.centerx, mouse_pos[1] - self.rect.centery
-        self.angle = 90 + math.degrees(math.atan2(dy, dx))
-
-background = Background(screen)
-octopus = Octopus(screen)
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-    mouse_pos = pygame.mouse.get_pos()
-    octopus.update(mouse_pos)
-    screen.fill((0, 0, 0))
-    background.draw(screen)
-    octopus.draw(screen)
-    pygame.display.flip()
     running = True
     while running:
         mouse_pos = pygame.mouse.get_pos()
@@ -129,7 +90,7 @@ while running:
         octopus.draw(screen)
         pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
